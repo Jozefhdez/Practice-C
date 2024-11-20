63bade28491265a9f8ce60ee26c7c0ff4dbd8443
Jozef David Hernández Campos <141695511+Jozefhdez@users.noreply.github.com>
2024-11-20 08:48:13 -0600

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,30 +1,28 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+volatile sig_atomic_t stop = 0;
 
-
-void noMeMates(int signum){
-   printf("Recibi la señal %d", signum);
-   printf("No me puedes matar");
+void no_me_mates(int signum) {
+    printf("RECIBI LA SEÑAL %d\n", signum);
+    printf("NO ME PUEDES MATAR\n");
 }
 
-
-int condicion;
-void terminarCiclo(int signum){
-   condicion = 0;
+void me_matas(int signum) {
+    printf("SEÑAL RECIBDA %d\n", signum);
+    printf("ME MATARON\n");
+    stop = 1;
 }
 
+int main() {
+    signal(2, no_me_mates);
+    signal(10, me_matas);
+    while (stop==0) {  
+        printf("TRABAJANDO\n");
+        sleep(1);
+    }
 
-
-
-int main(){
-   signal(2, noMeMates);
-   signal(10, terminarCiclo);
-   condicion = 1;
-   while(condicion == 1){
-       printf("Trabajando ");
-       sleep(2);
-   }
-   printf("Terminando");
-   return 0;
+    printf("FIN DEL PROGRAMA\n");
+    return 0;
 }
